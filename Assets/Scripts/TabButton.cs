d4da9116a1beba3973832e58010c6e024c000dc1
Alexis <alexisrobin21@gmail.com>
2023-05-29 10:23:52 +0200

    implement shop design

---
+++ Assets/Scripts/TabButton.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class TabButton : MonoBehaviour, IPointerClickHandler
+{
+    public TabGroup tabGroup;
+
+    public Color idleColor;
+    public Color selectedColor;
+
+    public Image image;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        tabGroup.OnTabSelected(this);
+    }
+
+    public void resetButton()
+    {
+        image.color = idleColor;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        image = GetComponent<Image>();
+        tabGroup.Subscribe(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
